a64fd01af7c133cb8bd65152867407e5b855919f
ctrl-gaurav <gauravhhh30@gmail.com>
2022-02-03 05:17:01 +0530

    Fixed Bugs

--- app.py
+++ app.py
@@ -15,12 +15,13 @@ app.static_folder = 'static'
 
 nltk.download('popular')
 lemmatizer = WordNetLemmatizer()
-intents = json.loads(open('Dataset/data.json').read())
+intents = json.loads(open('Dataset/sample_intents.json').read())
 words = pickle.load(open('NLP Trained Model/texts.pkl','rb'))
 classes = pickle.load(open('NLP Trained Model/labels.pkl','rb'))
 model = load_model('NLP Trained Model/model.h5')
 
 bot = Engine()
+run_model = run_model()
 
 @app.route("/")
 def home():
@@ -45,10 +46,14 @@ def upload():
         # Saving the file to ./uploads
         basepath = os.path.dirname(__file__)
 
+        current_directory = os.getcwd()
+        final_directory = os.path.join(current_directory, r'uploads')
+        if not os.path.exists(final_directory):
+            os.makedirs(final_directory)
+
         file_path = os.path.join(basepath, 'uploads', secure_filename(f.filename))
         f.save(file_path)
-
-        run_model = run_model()
+        
         result = run_model.model_predict(img_path=file_path)
 
         return str(result)
